472ab201dae3959f219adddf52bfdddcee16564b
Mi Songhua <1512224016@qq.com>
2024-04-03 11:44:28 +0800

    fix the original dataset

--- load.py
+++ load.py
@@ -6,7 +6,7 @@ with open('Tutor.csv', 'r', newline='') as infile:
     data = list(reader)  # 将数据读取为列表
 
 # creat tutor table
-tutor_table = [['name', 'education', 'institute', 'status']]
+tutor_table = [['tutor_name', 'education', 'institute', 'status']]
 tutor_primary_key = set()  # 用于检查是否有重复的 name
 
 for row in data[1:]:
@@ -16,17 +16,17 @@ for row in data[1:]:
         tutor_primary_key.add(name)
 
 # create lesson table
-lesson_table = [['course_number', 'tutor_name']]  # 包含课程号和老师名字
+subject_table = [['subject_code', 'tutor_name', 'school_name']]  # 包含课程号和老师名字
 course_set = set()  # 用于存储所有课程号
 
 for row in data[1:]:
-    name, _, _, lessons, _, _ = row
-    lessons = lessons.split(',')  # 将课程号分割成列表
-    for lesson in lessons:
-        if lesson.strip() != '':
-            course_number = lesson.strip()  # 去除课程号两端的空格
+    name, _, _, subjects, _, school_name = row
+    subjects = subjects.split(',')  # 将课程号分割成列表
+    for subject in subjects:
+        if subject.strip() != '':
+            course_number = subject.strip()  # 去除课程号两端的空格
             if course_number not in course_set:
-                lesson_table.append([course_number, name])
+                subject_table.append([course_number, name, school_name])
                 course_set.add(course_number)
 
 # create school table
@@ -44,9 +44,9 @@ with open('tutor_table.csv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
     writer.writerows(tutor_table)
 
-with open('lesson_table.csv', 'w', newline='') as outfile:
+with open('subject_table.csv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
-    writer.writerows(lesson_table)
+    writer.writerows(subject_table)
 
 with open('school_table.csv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
